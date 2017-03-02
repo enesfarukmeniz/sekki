73d7801734ec1481de2dcd6e20234deac519d2a0
Enes Faruk Meniz <EnesFaruk.Meniz@telenity.com>
2017-03-02 10:39:12 +0300

    added description del command finished deladd added save added with some config change

--- src/commands.js
+++ src/commands.js
@@ -4,27 +4,22 @@ const base64 = require('node-base64-image');
 const config = require('./../config.json');
 
 const commands = {
-    permtest: {
-        permissions: ["KICK_MEMBERS", "BAN_MEMBERS"],
-        guild: true,
-        help: "permtest",
-        run: function (client, message) {
-        }
-    },
     help: {
         help: "help [%command%](obviously lol)",
+        description: "Helps about commands lol",
         run: function (client, message, [command]) {
             message.delete();
             if (command) {
                 if (!commands[command]) {
                     message.channel.sendMessage(`**${command}** is not valid command.`);
                 } else {
-                    message.channel.sendMessage(`\`${command} command usage: ${config.prefix}${commands[command].help}\``)
+                    message.channel.sendMessage(`\`\`\`${command} command usage: ${config.prefix}${commands[command].help}\nIt does: ${commands[command].description}\`\`\``);
                 }
             } else {
                 let helpMessage = "```fix\n";
                 Object.keys(commands).forEach(key => {
                     helpMessage += "Command: " + key + "\nUsage: " + config.prefix + commands[key].help;
+                    helpMessage += "\nWhat: " + commands[key].description;
                     helpMessage += "\nGuild Only: " + (commands[key].guild === true ? "TRUE" : "FALSE");
                     helpMessage += "\nPermissions: " + (commands[key].permissions && commands[key].permissions.length ? commands[key].permissions.join(", ") : "NONE");
                     helpMessage += "\n\n";
@@ -36,6 +31,7 @@ const commands = {
     },
     ping: {
         help: "ping",
+        description: "ping pong",
         run: function (client, message) {
             message.delete();
             message.channel.sendMessage("ping...")
@@ -46,12 +42,14 @@ const commands = {
     },
     github: {
         help: "github",
+        description: "Returns github address of project",
         run: function (client, message) {
             message.edit("https://github.com/enesfarukmeniz/sekki");
         }
     },
     reply: {
         help: "reply %message_id% %message%",
+        description: "Replies a message with id",
         run: function (client, message, [messageId, ...messageArray]) {
             message.channel.fetchMessages({limit: 1, around: messageId}).then(messages => {
                 const replyMessage = messages.first();
@@ -70,7 +68,8 @@ const commands = {
         }
     },
     mreply: {
-        help: "mreply %message_id% %message% (replies with mention)",
+        help: "mreply %message_id% %message%",
+        description: "Replies a message with id and mentions message owner",
         run: function (client, message, [messageId, ...messageArray]) {
             message.channel.fetchMessages({limit: 1, around: messageId}).then(messages => {
                 const replyMessage = messages.first();
@@ -90,7 +89,8 @@ const commands = {
     },
     emojify: {
         permissions: [],
-        help: "emojify (with an image)",
+        help: "emojify",
+        description: "Shows how image looks like if becomes emoji. Use command with image",
         run: function (client, message) {
             //TODO - beauty and logging
             if (!message.attachments.array().length) {
@@ -103,10 +103,10 @@ const commands = {
                     base64.encode(image.url, {}, function (err, response) {
                         sharp(response).resize(32, 32).png().toBuffer(function (err, buffer, info) {
                             message.channel.sendFile(buffer, "image.png", "Big Emoji");
-                        })
-                        sharp(response).resize(24, 24).png().toBuffer(function (err, buffer, info) {
+                        });
+                        sharp(response).resize(22, 22).png().toBuffer(function (err, buffer, info) {
                             message.channel.sendFile(buffer, "image.png", "Small Emoji");
-                        })
+                        });
                     });
 
                 }
@@ -115,30 +115,79 @@ const commands = {
     },
     del: {
         help: "del [%number%]",
-        run: function (client, message, [number]) {
-            if (number) {
-                //TODO
-            } else {
-                message.delete().then(() => message.channel.fetchMessages({
-                    limit: 5
-                }).then(messages => {
-                    const [msg, ...msgs] = messages.array().filter(msg => msg.author.id === client.user.id);
-                    if (msg) {
+        description: "Deletes user's own messages. Default %number% value is 1, max is 100. Not guaranteed to delete %number% of messages cause of discord api limitations",
+        run: function (client, message, [number = 1]) {
+            message.delete().then(() => message.channel.fetchMessages({
+                limit: 100
+            }).then(messages => {
+                let msgs = messages.filterArray(msg => msg.author.id === client.user.id).slice(0, number);
+                if (msgs.length) {
+                    msgs.forEach(msg => {
+                        //TODO log
                         msg.delete();
-                    } else {
-                        //TODO log?
-                    }
-                }));
-            }
+                    });
+                } else {
+                    //TODO log?
+                }
+            }));
+        }
+    },
+    delall: {
+        help: "delall [%number%]",
+        permissions: ["MANAGE_MESSAGES"],
+        description: "Deletes all users' messages. Default %number% value is 1, max 100",
+        run: function (client, message, [number = 1]) {
+            message.delete().then(() => message.channel.fetchMessages({
+                limit: 100
+            }).then(messages => {
+                let msgs = messages.array().slice(0, number);
+                if (msgs.length) {
+                    msgs.forEach(msg => {
+                        //TODO log
+                        msg.delete();
+                    });
+                } else {
+                    //TODO log?
+                }
+            }));
+        }
+    },
+    save: {
+        help: "save %messageid%",
+        description: "Save message with %messageid% to configured saveChannel",
+        run: function (client, message, [messageId, ..._]) {
+            message.delete();
+            message.channel.fetchMessages({limit: 1, around: messageId}).then(messages => {
+                const saveMessage = messages.first();
+                const saveChannel = client.channels.filter(channel => config.saveChannel == channel.id).first();
+                if (saveChannel) {
+                    saveChannel.sendMessage("", {
+                        embed: {
+                            color: 0x7289DA,
+                            author: {
+                                name: saveMessage.author.username,
+                                icon_url: saveMessage.author.avatarURL
+                            },
+                            description: saveMessage.content,
+                            timestamp: saveMessage.createdAt,
+                            footer: {
+                                text: saveMessage.guild ? (`${saveMessage.guild.name} : #${saveMessage.channel.name}`) : "Private",
+                                icon_url: saveMessage.guild ? saveMessage.guild.iconURL : ""
+                            }
+                        }
+                    });
+                } else {
+                    //TODO log no saveChannel
+                }
+            });
         }
     }
     //TODO list
     //logging to a channel
-    //del [number]
-    //delall
+    //save message (to a channel?)
     //info
     //eval!!!
-    //lennyface - node-json-db
+    //lenny face - node-json-db
     //
 };
 
--- src/logger.js
+++ src/logger.js
@@ -2,8 +2,15 @@ const winston = require("winston");
 require("winston-daily-rotate-file");
 //TODO lots of work here...
 const logger = {
-    mention: function(message) {
-        //console.log(arguments)
+    mention: function (client, message) {
+        let log = "";
+        if (message.guild) {
+            log += `Guild: ${message.guild.name}\n`;
+            log += `Channel: ${message.channel.name}\n`;
+        }
+        log += `Timestamp: ${message.createdAt}\n`;
+        log += `Message: ${message.author.username}: ${message.cleanContent}\n`;
+        console.log(log);
     },
     noCommand: function() {
         //console.log(arguments)
